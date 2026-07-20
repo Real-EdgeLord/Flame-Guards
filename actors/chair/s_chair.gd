effa3b1ab2c565990058b251e41c0c504fa92655
Edge Lord <131451981+Real-EdgeLord@users.noreply.github.com>
2026-07-20 14:21:30 +0200

    Add chair (#14)
    * added support for android'
    
    * started work on chair

--- actors/chair/s_chair.gd
+++ actors/chair/s_chair.gd
@@ -1,2 +1,26 @@
 extends Node2D
 class_name Chair
+
+
+@export var back: Sprite2D
+@export var seat: Sprite2D
+@export var legs: Sprite2D
+@export var color: Color
+
+func set_back(back_piece : ChairPiece) -> void :
+	
+	pass
+
+
+func set_seat(seat_piece : ChairPiece) -> void :
+	
+	pass
+
+
+func set_legs(leg_piece : ChairPiece) -> void :
+	
+	pass
+
+
+func destory_chair() -> void :
+	queue_free()
---
+++ actors/chair_piece/legs/s_chair_legs.gd
@@ -0,0 +1,2 @@
+extends ChairPiece
+class_name ChairLegs
